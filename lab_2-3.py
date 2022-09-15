3314e658b8434767de8b72172828b2b0c5378aad
Tanner Langis <p23tlangis@fairfieldprep.org>
2022-09-15 10:39:33 -0400

    Lab 2-3 Submission

---
+++ lab_2-3.py
@@ -0,0 +1,13 @@
+#Author TJL 9/15/22
+a=2 
+print (a)
+print (type(a))
+a = float(a)
+print(a)
+print (type(a))
+a = str(a)
+print(type(a))
+a = bool(a)
+print(type(a))
+print(a == 2) #it says it is false because since i changed it to a bool 2.0 does not equal 2.
+
