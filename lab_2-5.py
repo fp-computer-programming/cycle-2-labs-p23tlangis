bdfd4dd06614afd42464617efd9c3647c29f6bf9
Tanner Langis <p23tlangis@fairfieldprep.org>
2022-09-19 12:05:45 -0400

    Lab 2-5 Submission

---
+++ lab_2-5.py
@@ -0,0 +1,33 @@
+#Author TJL 9/19/22
+
+
+
+
+x=21
+#x+= 5
+x= x+5 
+print(x)
+y=21
+#y= y-3
+y-= 3
+print(y)
+z=21
+#z**= 2
+z= z**2
+print(z)
+w=21
+#w= w%2
+w%= 2
+print(w)
+t=21
+#t= t/5
+t/= 5
+print(t)
+a=21
+#a *=12
+a= a*12
+print(a)
+i=21
+#i= i//7
+i//=7
+print(i)
