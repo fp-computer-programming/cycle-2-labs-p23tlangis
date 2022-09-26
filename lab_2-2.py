a2e39d3e17d61b7e8abcc1bab7e3ae3d19eea6ff
Tanner Langis <p23tlangis@fairfieldprep.org>
2022-09-26 00:21:36 -0700

    Lab 2-2 Submission

---
+++ lab_2-2.py
@@ -0,0 +1,13 @@
+#Author TJL 9/12/22
+a=3
+b=a
+print(a)
+print(b)
+#a=3 because I set the variable a equal to 3
+#b=3 because I set b equal to a and a equals 3
+a=7
+b=a
+print(a)
+print(b)
+#a now equals 7 because i set a to 7
+#b now equals 7 because b equals a and a equals 7
