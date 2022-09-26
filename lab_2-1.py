e8c2b50a71426ddcd545f59423cc3d52bb6160de
Tanner Langis <p23tlangis@fairfieldprep.org>
2022-09-26 00:21:16 -0700

    Lab 2-1 Submission

---
+++ lab_2-1.py
@@ -0,0 +1,18 @@
+#Author TJL 9/12/22
+print(123)
+print(+321)
+print(-321)
+print(20+20)
+print(2020-4)
+print(20+20)
+print(2*4*6*8)
+print(9/5)
+print(9//5)
+print(13/0)
+print(3%10)
+print(3%10)
+print(2**3)
+print(2*2*2)
+print(0/0)
+
+  
