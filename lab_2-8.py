df5603f11a573439c8cf85994e2777a08c46ad6c
Tanner Langis <p23tlangis@fairfieldprep.org>
2022-09-26 00:23:08 -0700

    Lab 2-8 Submission

---
+++ lab_2-8.py
@@ -0,0 +1,14 @@
+#Author TJL 9/23/22
+x=9
+
+if x>=15:
+    print("You got a gold medal")
+else:
+    if x<=14:
+        print("You got a silver medal")
+    else:
+        if x<=11:
+            print("You got a bronze medal")
+        else:
+            if x<=8:
+                print("You got no medal")
