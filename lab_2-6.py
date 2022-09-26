3e8db2eeaa08271db4e3a7cf9d45e299da31769c
Tanner Langis <p23tlangis@fairfieldprep.org>
2022-09-26 00:22:01 -0700

    Lab 2-6 Submission

---
+++ lab_2-6.py
@@ -0,0 +1,23 @@
+#Author TJL 9/25/22
+a=5
+b=10
+c=0
+d=-4
+if a> 0:
+    print("a is a positve number")
+if a< 0:
+    print("a is a negative number")
+if b> 0:
+    print("b is a positive number")
+if b< 0:
+    print("b is a negative number")
+if c> 0:
+    print("c is a positive number")
+if c< 0: 
+    print("c is a negative number")
+if c== 0:
+    print("c equals Zero")
+if d> 0:
+    print("d is a postive number")
+if d< 0:
+    print("d is a negative number")
