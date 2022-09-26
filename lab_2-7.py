1b9d46de303e87abffe66d38588f2779e2b996a7
Tanner Langis <p23tlangis@fairfieldprep.org>
2022-09-26 00:22:42 -0700

    Lab 2-7 Submission

---
+++ lab_2-7.py
@@ -0,0 +1,51 @@
+#Author TJL 9/25/22
+a=5
+b=10
+c=0
+d=-4
+if a> 0:
+    print("a is a positve number")
+else:
+    print("a is a negative number")
+if b> 0:
+    print("b is a positive number")
+else:
+    print("b is a negative number")
+if c> 0:
+    print("c is a positive number")
+if c==0:
+    print("c equals zero")
+else: 
+    print("c is a negative number")
+if d> 0:
+    print("d is a postive number")
+else:
+    print("d is a negative number")
+
+if a> 5:
+    print("a is more than 5")
+if a< 5:
+    print("a is less than -5")
+else:
+    print("a is between 5 and -5")
+
+if b< 5:
+    print("b is less than -5")
+if b> 5:
+    print("b is more than 5")
+else:
+    print("b is between 5 and -5")
+
+if c> 5:
+    print("c is more than 5")
+if c< 5:
+    print("c is less than -5")
+else:
+    print("c is between 5 and -5")
+
+if d> 5:
+    print("d is more than 5")
+if d< 5:
+    print("d is less than -5")
+else:
+    print("d is between 5 and -5")
